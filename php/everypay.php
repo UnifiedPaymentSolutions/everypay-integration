2a03509eb888b01fc8daade3cecc74e79f4e997e
Said Kaldybaev <said.kaldybaev@every-pay.com>
2015-07-28 18:06:43 +0300

    added cc_last_four_digist and option to pass hmac_fields

--- php/everypay.php
+++ php/everypay.php
@@ -21,40 +21,45 @@ class Everypay
     }
 
     /**
-     * Populates and returns array of fields to be submitted
-     * for payment.
-     *
-     * Expects following data as input:
-     * array(
-     *  'account_id' => account id,
-     *  'amount' => amount to pay,
-     *  'billing_address' => billing address street name,
-     *  'billing_city' => billing address city name,
-     *  'billing_country' => billing address 2 letter country code,
-     *  'billing_postcode' => billing address postal code,
-     *  'callback_url' => callback url,
-     *  'customer_url' => return url,
-     *  'delivery_address' => shipping address street name,
-     *  'delivery_city' => shipping address city name,
-     *  'delivery_country' => shipping address 2 letter country code,
-     *  'delivery_postcode' => shipping address postal code,
-     *  'email' => customer email address,
-     *  'order_reference' => order reference number,
-     *  'user_ip' => user ip address,
-     * );
-     *
-     * @param $data array
-     * @param $language string
-     *
-     * @return Array
-     */
+      * Populates and returns array of fields to be submitted
+      * for payment.
+      *
+      * Expects following data as input:
+      * array(
+      *  'account_id' => account id,
+      *  'amount' => amount to pay,
+      *  'billing_address' => billing address street name,
+      *  'billing_city' => billing address city name,
+      *  'billing_country' => billing address 2 letter country code,
+      *  'billing_postcode' => billing address postal code,
+      *  'callback_url' => callback url,
+      *  'customer_url' => return url,
+      *  'delivery_address' => shipping address street name,
+      *  'delivery_city' => shipping address city name,
+      *  'delivery_country' => shipping address 2 letter country code,
+      *  'delivery_postcode' => shipping address postal code,
+      *  'email' => customer email address,
+      *  'order_reference' => order reference number,
+      *  'user_ip' => user ip address,
+      *  'hmac_fields' => string which contains all fields (keys) that are going to be used in hmac calculation, separated by comma.
+      * );
+      *
+      * @param $data array
+      * @param $language string
+      *
+      * @return Array
+    */
 
-    public function getFields(array $data, $language = 'en')
+    public function getFields(array $data, $language = 'en', $hmac_fields = false)
     {
         $data['api_username'] = $this->api_username;
         $data['nonce'] = $this->getNonce();
         $data['timestamp'] = time();
         $data['transaction_type'] = 'authorisation';
+
+        if ($hmac_fields == true) {
+            $data['hmac_fields'] = implode(',', array_keys($data)) . ",hmac_fields";
+        }
 
         $data['hmac'] = $this->signData($this->prepareData($data));
         $data['locale'] = $language;
@@ -63,73 +68,78 @@ class Everypay
     }
 
     /**
-     * Verifies return data
-     *
-     * Expects following data as input:
-     *
-     * for successful and failed payments:
-     *
-     * array(
-     *  'account_id' => account id in Everypay system
-     *  'amount' => amount to pay,
-     *  'api_username' => api username,
-     *  'nonce' => return nonce
-     *  'order_reference' => order reference number,
-     *  'payment_reference' => payment reference number,
-     *  'payment_state' => payment state,
-     *  'timestamp' => timestamp,
-     *  'transaction_result' => transaction result
-     * );
-     *
-     * for cancelled payments:
-     *
-     * array(
-     *  'api_username' => api username,
-     *  'nonce' => return nonce
-     *  'order_reference' => order reference number,
-     *  'payment_state' => payment state,
-     *  'timestamp' => timestamp,
-     *  'transaction_result' => transaction result
-     * );
-     *
-     * @param $data array
-     *
-     * @return int 1 - verified successful payment, 2 - verified cancelled payment, 3 - verified failed payment
-     * @throws Exception
-     */
-    
+      * Verifies return data
+      *
+      * Expects following data as input:
+      *
+      * for successful and failed payments:
+      *
+      * array(
+      *  'account_id' => account id in Everypay system
+      *  'amount' => amount to pay,
+      *  'api_username' => api username,
+      *  'nonce' => return nonce
+      *  'order_reference' => order reference number,
+      *  'payment_reference' => payment reference number,
+      *  'payment_state' => payment state,
+      *  'timestamp' => timestamp,
+      *  'transaction_result' => transaction result
+      * );
+      *
+      * for cancelled payments:
+      *
+      * array(
+      *  'api_username' => api username,
+      *  'nonce' => return nonce
+      *  'order_reference' => order reference number,
+      *  'payment_state' => payment state,
+      *  'timestamp' => timestamp,
+      *  'transaction_result' => transaction result
+      * );
+      *
+
+      * @param $data array
+      *
+      * @return int 1 - verified successful payment, 2 - verified cancelled payment, 3 - verified failed payment
+      * @throws Exception
+    */
+
     public function verify(array $data)
     {
         if ($data['api_username'] !== $this->api_username)
             throw new Exception('Invalid username.');
 
         $now = time();
-        if (($data['timestamp'] > $now) || ($data['timestamp'] < ($now - 300)))
+        if (($data['timestamp'] > $now) || ($data['timestamp'] < ($now - 600)))
             throw new Exception('Response outdated.');
 
         //  if ($data['order_reference'] != $this->getOrderReference())
         //      throw new Exception('Order number doesn\'t match.');
-        
+
         /**
-         * Refer to the Integration Manual for more information about 'order_reference' validation.
-         */ 
-     
+          * Refer to the Integration Manual for more information about 'order_reference' validation.
+        */
+
         if (!$this->verifyNonce($data['nonce']))
             throw new Exception('Nonce is already used.');
-        
+
         /**
-         * Refer to the Integration Manual for more information about 'nonce' uniqueness validation.
-         */ 
-     
+          * Refer to the Integration Manual for more information about 'nonce' uniqueness validation.
+        */
+
         $status = $this->statuses[$data['transaction_result']];
         // common data
         $verify = array(
-            'api_username' => $data['api_username'],
-            'nonce' => $data['nonce'],
-            'order_reference' => $data['order_reference'],
-            'payment_state' => $data['payment_state'],
-            'timestamp' => $data['timestamp'],
-            'transaction_result' => $data['transaction_result']
+            'api_username'        => $data['api_username'],
+            'nonce'               => $data['nonce'],
+            'order_reference'     => $data['order_reference'],
+            'payment_state'       => $data['payment_state'],
+            'timestamp'           => $data['timestamp'],
+            'transaction_result'  => $data['transaction_result'],
+            'account_id'          => $data['account_id'],
+            'amount'              => $data['amount'],
+            'payment_reference'   => $data['payment_reference'],
+            'cc_last_four_digits' => $data['cc_last_four_digits']
         );
 
         switch ($data['transaction_result'])
@@ -144,10 +154,6 @@ class Everypay
                 if (isset($data['processing_warnings'])) {
                     $verify['processing_warnings'] = $data['processing_warnings'];
                 }
-
-                $verify['account_id'] = $data['account_id'];
-                $verify['amount'] = $data['amount'];
-                $verify['payment_reference'] = $data['payment_reference'];
                 break;
             case 'cancelled':
                 break;
@@ -173,11 +179,11 @@ class Everypay
     //  abstract protected function getOrderReference();
 
     /**
-     * Prepare data package for signing
-     *
-     * @param array $data
-     * @return string
-     */
+      * Prepare data package for signing
+      *
+      * @param array $data
+      * @return string
+    */
 
     private function prepareData(array $data)
     {
