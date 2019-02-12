0b9be74a74a3dc20d580d3ebeae9d6f3c133ebcb
Laas Toom <laas.toom@gmail.com>
2019-02-12 15:37:22 +0200

    APP-2133: Refactor JS functions to use Object Pattern

--- javascript/JavaScript.js
+++ javascript/JavaScript.js
@@ -1,18 +1,43 @@
-/* An example of an iframe:
+/*
 
+// USAGE
+// ======
+//
+// 0. Include jQuery
+
+// 1. Include this file (e.g. <script src="...">)
+
+// 2.Assign selector to the iframe
+EveryPay.iframe = jQuery('#iframe-payment-container iframe')
+
+// 3. Assign callback to get transaction result from EveryPay message
+EveryPay.messageCallback = function(message){
+  // An example of a possible way to process message data. Note, all fields, except transaction_result, are locale dependent -
+  // the texts are returned in the language specified in the API request locale parameter.
+  if (message.transaction_result) {
+    jQuery('.transaction_result').append(message.transaction_result); // Transaction's result. Possible states: completed, failed.
+    jQuery('.message_title').append(message.message_title);           // Contains payment result page title - "Thank you! Payment successful." or "Sorry, payment was unsuccessful."
+    jQuery('.message_error').append(message.message_error);           // Contains information about the cause of payment's failure, if the exact reason is known and can be revealed to the buyer.
+    jQuery('.message_action').append(message.message_action);         // Contains suggestions about what the customer should do to be able to perform a successful payment.
+    jQuery('.message_contact').append(message.message_contact);       // Contains instructions about who the customer should contact if the problem persists.
+  }
+}
+
+// An example of an iframe
+// N.B. iframe-payment-container is used as a placeholder during the time when the iframe is expanded.
 <div id="iframe-payment-container" style="border: 0px; min-width: 460px; min-height: 325px">
   <iframe width="460" height="325" style="border: 0px; height: 325; width: 460"></iframe>
 </div>
 
-N.B. iframe-payment-container is used as a placeholder during the time when the iframe is expanded.
 */
 var EveryPay = {
-  shrinkIframe: function(iframe, iframe_data) {
-    iframe.css(iframe_data);
+  shrinkIframe: function(iframe) {
+    iframe.css(this.shrinkedIframeStyle);
     jQuery("#dimmed_background_box").remove();
   },
+
   expandIframe: function(iframe) {
-    var iframe_data = {
+    this.shrinkedIframeStyle = {
       position: iframe.attr("position") || "static",
       top: iframe.position().top,
       left: iframe.position().left,
@@ -32,39 +57,35 @@ var EveryPay = {
       iframe.css({ height:640,width:960,top:(jQuery(window).height()-640)/2 });
     }
     iframe.css({ position:'fixed',zIndex:9999,margin:'auto' });
-    return iframe_data;
-  }
+  },
+
+  messageHandler: function(event) {
+    if (event.origin !== "https://igw-demo.every-pay.com" && event.origin !== "https://pay.every-pay.eu") { return; }
+
+    var message = JSON.parse(event.data);
+    /*
+    1. An "expand" message is sent from the iframe page when 3D secure page is going to be displayed.
+      The size of the iframe should be adjusted to hold 3D secure page
+    2. A "shrink" message is sent from the iframe page when a user has provided authorisation details on the 3D secure page.
+      The size of the iframe should be set to the initial values
+    */
+    if (message.resize_iframe == "expand") {
+      this.expandIframe(this.iframe);
+    } else if (message.resize_iframe == "shrink") {
+      this.shrinkIframe(this.iframe);
+    }
+
+    if (typeof this.messageCallback === "function"){
+      this.messageCallback(message)
+    }
+  },
+
+  shrinkedIframeStyle: "",
+  messageCallback: null,
+  iframe: jQuery('#iframe-payment-container iframe'), // assign jQuery selector to the iframe
 };
-var EveryPayAvailables = {
-  shrinked_iframe_data: "",
-  iframe: jQuery('#iframe-payment-container iframe') // iframe selector should be used
-};
 
-window.addEventListener('message', function(event) {
-  if (event.origin !== "https://igw-demo.every-pay.com" && event.origin !== "https://pay.every-pay.eu") { return; }
+// Add event listener to the 'message' event
+window.addEventListener('message', EveryPay.messageHandler.bind(EveryPay), false)
 
-  var message = JSON.parse(event.data);
-  /*
-  1. An "expand" message is sent from the iframe page when 3D secure page is going to be displayed.
-     The size of the iframe should be adjusted to hold 3D secure page
-  2. A "shrink" message is sent from the iframe page when a user has provided authorisation details on the 3D secure page.
-     The size of the iframe should be set to the initial values
-  */
-  if (message.resize_iframe == "expand") {
-    EveryPayAvailables.shrinked_iframe_data = EveryPay.expandIframe(EveryPayAvailables.iframe);
-  } else if (message.resize_iframe == "shrink") {
-    EveryPay.shrinkIframe(EveryPayAvailables.iframe, EveryPayAvailables.shrinked_iframe_data);
-  }
 
-  // An example of a possible way to process message data. Note, all fields, except transaction_result, are locale dependent -
-  // the texts are returned in the language specified in the API request locale parameter.
-  //
-  // if (message.transaction_result) {
-  //   jQuery('.transaction_result').append(message.transaction_result); // Transaction's result. Possible states: completed, failed.
-  //   jQuery('.message_title').append(message.message_title);           // Contains payment result page title - "Thank you! Payment successful." or "Sorry, payment was unsuccessful."
-  //   jQuery('.message_error').append(message.message_error);           // Contains information about the cause of payment's failure, if the exact reason is known and can be revealed to the buyer.
-  //   jQuery('.message_action').append(message.message_action);         // Contains suggestions about what the customer should do to be able to perform a successful payment.
-  //   jQuery('.message_contact').append(message.message_contact);       // Contains instructions about who the customer should contact if the problem persists.
-  // }
-  //
-}, false);
